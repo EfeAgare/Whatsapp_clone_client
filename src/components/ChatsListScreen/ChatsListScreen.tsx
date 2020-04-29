a9d3d3f0f3b191e1870f1c78d2b7a7a62982169f
Efe <knowledgeagare157@gmail.com>
2020-04-29 17:43:01 +0100

    adding css styling to the application

--- src/components/ChatsListScreen/ChatsListScreen.tsx
+++ src/components/ChatsListScreen/ChatsListScreen.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ChatsNavbar } from './ChatsNavbar';
 import ChatsList from './ChatsList';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  height: 100vh;
+`;
 export const ChatsListScreen = () => {
   return (
-    <div>
+    <Container>
       <ChatsNavbar />
       <ChatsList />
-    </div>
+    </Container>
   );
 };
